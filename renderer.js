f19723faff4c06c8ca57df18306c119ccc99a744
Brian Zindler <zindlerb@gmail.com>
2016-11-15 18:16:13 -0800

    react

--- renderer.js
+++ renderer.js
@@ -1,3 +1,18 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+var React = require('react');
+var ReactDom = require('react-dom');
+
+var App = React.createClass({
+    render: function () {
+        return 
+    }
+});
+
+ReactDom.render(
+    App,
+    document.getElementById('root')
+);
+
+
