604fbdb957d3b4621bb917fad599dda9d7e83fa3
Brian Zindler <zindlerb@gmail.com>
2016-11-15 19:14:20 -0800

    test out babel browserify

---
+++ app/app.js
@@ -0,0 +1,23 @@
+// ES6 Component
+// Import React and ReactDOM
+import React from 'react';
+import ReactDOM from 'react-dom';
+// Search component created as a class
+class Search extends React.Component {
+
+    // render method is most important
+    // render method returns JSX template
+    render() {
+        return (
+            <form>
+                <input type = "text" />
+                <input type = "submit" />
+            </form>
+        );
+    }
+}
+
+// Render to ID content in the DOM
+ReactDOM.render( < Search / > ,
+                 document.getElementById('content')
+);
--- main.js
+++ main.js
@@ -1,8 +1,14 @@
+'use strict';
+
 const electron = require('electron')
 // Module to control application life.
 const app = electron.app
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
+
+// For reloading in dev
+require('electron-reload')(__dirname);
+
 
 const path = require('path')
 const url = require('url')
