2ac49ea70ac8c26d72c72be4c400aa5bd5b5dfc2
Brian Zindler <zindlerb@gmail.com>
2017-02-22 18:37:44 -0800

    clean up tests file

--- __tests__/actions.js
+++ __tests__/actions.js
@@ -1,20 +1,11 @@
-// Lookup best way to test reducer redux.
-// Do after making state immutable
-
 var _ = require('lodash');
 
 var { getNewStore, actions } = require('../app/stateManager.js');
 var { componentTypes } = require('../app/constants.js');
 var { container, ComponentsContainer } = require('../app/base_components.js');
 
-// Requires mocking the file save...
-
 describe('make component block', () => {
   var store = getNewStore();
-
-  // add component
-  // make component into component block
-  // confirm that name works
   const state = store.getState();
   const currentPageId = state.get('currentPageId');
   const rootComponentTreeId = state.getIn([
